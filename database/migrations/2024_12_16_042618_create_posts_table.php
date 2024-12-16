76efd59dc5cd9f9e9f0504a66e5bc1371497a388
Chris Mellor <cmellor@gmail.com>
2024-12-16 11:38:51 +0700

    feat: Add required fields to migrations

--- database/migrations/2024_12_16_042618_create_posts_table.php
+++ database/migrations/2024_12_16_042618_create_posts_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(\App\Models\User::class)->constrained();
+            $table->foreignIdFor(\App\Models\Category::class)->constrained();
+            $table->string('title');
+            $table->text('content');
+            $table->date('published_at');
             $table->timestamps();
         });
     }
