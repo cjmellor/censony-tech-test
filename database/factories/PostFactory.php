1f9f8b367b2a2a8938dfdc4ba1523bfb3634497c
Chris Mellor <cmellor@gmail.com>
2024-12-16 12:13:51 +0700

    feat: Add Factory/Seeder data

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,8 @@
 
 namespace Database\Factories;
 
+use App\Models\Category;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -17,7 +19,11 @@ class PostFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
+            'title' => $this->faker->sentence,
+            'content' => $this->faker->paragraphs(3, true),
+            'published_at' => $this->faker->dateTimeBetween('-1 year', 'now'),
         ];
     }
 }
